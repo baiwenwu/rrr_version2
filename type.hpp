b3e0a53e731144590d040f223df2bf1eb6c3331c
zhe wang <1024545641@qq.com>
2014-04-19 19:57:16 +0800

    public data type

---
+++ type.hpp
@@ -0,0 +1,19 @@
+/*定义全局数据类型*/
+
+#ifndef _TYPE_H
+#define _TYPE_H
+
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
+//字长
+#define D 64
+//rrr分块
+#define B 15
+#endif
